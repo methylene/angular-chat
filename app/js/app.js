1c5a4da65cb3bc412ab85743e0131a1e51977b77
methylene <theairspeedindicator@yahoo.com>
2012-12-07 00:10:39 +0000

    add reset.css, canvas, timeouts

--- app/js/app.js
+++ app/js/app.js
@@ -5,9 +5,16 @@ var app = angular.module("chatApp", []).factory("firebaseService", function($tim
     var messageholder = {"messages":{}};
     ref.on("value", function(messageSnapshot) {
         $timeout(function() {
+            alert('0');
             messageholder.messages = messageSnapshot.val();
+            alert('1');
         }, 0);
     });
+    setTimeout(function(){
+        alert('3');
+        messageholder.messages = [];
+        alert('4');
+    }, 5000);
     return {
         "messageholder": messageholder,
         "addMessage":function(name, text) {
