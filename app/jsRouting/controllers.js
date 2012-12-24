807ca1e233d9bebdf54aadfa7ff5e8d6f53d8d31
methylene <theairspeedindicator@yahoo.com>
2012-12-24 09:18:17 +0000

    add quotes around function name

--- app/jsRouting/controllers.js
+++ app/jsRouting/controllers.js
@@ -1,9 +1,9 @@
 angular.module('routingApp', []).
     config(function($routeProvider) {
         $routeProvider.
-            when('/about', {templateUrl:'partials/about.html', controller:AboutCtrl}).
-            when('/experiments', {templateUrl:'partials/experiments.html', controller:ExperimentsCtrl   }).
-            when('/home', {templateUrl:'partials/home.html', controller:HomeCtrl   }).
+            when('/about', {templateUrl:'partials/about.html', controller:'AboutCtrl'}).
+            when('/experiments', {templateUrl:'partials/experiments.html', controller:'ExperimentsCtrl'   }).
+            when('/home', {templateUrl:'partials/home.html', controller:'HomeCtrl'   }).
             otherwise({redirectTo:'/home'});
     });
 
