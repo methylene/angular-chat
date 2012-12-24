7670e08259fc83d3bbe78b534e094e2b9dc5294e
methylene <theairspeedindicator@yahoo.com>
2012-12-24 13:20:31 +0000

    add nodejs server

---
+++ server/server.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+var util = require('util'),
+    fs = require('fs'),
+    url = require('url'),
+    express = require('express')
+
+var DEFAULT_PORT = 3000;
+
+app = express();
+
+app.get('*', function (req, res) {
+	res.writeHead(200, { "Content-Type": "application/json" });   
+	res.write(JSON.stringify(
+    {"days":[
+        {  "day":"Monday",
+            "hours":[
+                {
+                    "start":8,
+                    "end":12
+                }
+            ]
+        },
+        {  "day":"Tuesday",
+            "hours":[
+                {
+                    "start":8,
+                    "end":12
+                }
+            ]
+
+        },
+        {  "day":"Wednesday",
+            "hours":[
+                {
+                    "start":8,
+                    "end":12
+                }
+            ]
+
+        },
+        {  "day":"Thursday",
+            "hours":[
+                {
+                    "start":8,
+                    "end":12
+                }
+           ] 
+        }
+    ]}));
+	res.end();
+});
+
+app.listen(DEFAULT_PORT);
+console.log('Listening on port: ' + DEFAULT_PORT);
