09a9784efdbf7288bc288ce29504d77b3524d101
methylene <theairspeedindicator@yahoo.com>
2012-12-31 09:00:52 +0000

    add humanMsg

--- app/js/dragDrop3.js
+++ app/js/dragDrop3.js
@@ -1,21 +1,48 @@
 (function () {
-
     var app = angular.module('dragDrop3App', []);
 
-    app.controller('dragDrop3Ctrl', ['$scope', function ($scope) {
-        $scope.list1 = [
-            {name:'--reject--', reject:true},
-            {name:'Is'},
-            {name:'teh'},
-            {name:'@wesome'}
-        ];
-
-        $scope.list2 = [
-            {name:'AngularJS'},
-        ];
+    app.controller('dndCtrl', ['$scope', function ($scope) {
+        $scope.someArrays = {
+            list0:[
+                {name:'AngularJS'},
+                {name:'Is'},
+                {name:'teh'},
+                {name:'@wesome'}
+            ],
+            list1:[
+                {name:'AngularJS'}
+            ],
+            list2:[
+                {name:'Is'},
+                {name:'rather good'}
+            ],
+            list3:[
+                {name:'@wesome'},
+                {name:'MooTools'}
+            ]};
+        // args 'event' and 'ui' are pass-through from jQuery UI droppable callback. aSrc[index] == item.
+        // any truthy return value will abort the move action and gets passed to the onDropRejected callback
+        $scope.isDropForbidden = function (eSrc, eTarget, eDraggable, event, ui, aSrc, aTarget, item, index) {
+            if (aTarget.some(function (i) {
+                return i.name == item.name;
+            })) {
+                return {reason:"target already contains '" + item.name + "'"};
+            } else {
+                return false;
+            }
+        };
+        // aSrc[index] == item; 'error' is return value of isDropForbidden
+        $scope.onDropRejected = function (eSrc, eTarget, eDraggable, event, ui, aSrc, aTarget, item, index, error) {
+            humanMsg.displayMsg('Operation not permitted: ' + error.reason);
+        };
+        // When this gets called, the move operation is already completed,
+        // so aSrc[index] != item in general. 'index' is the original index the item had in aSrc
+        $scope.onDropComplete = function (eSrc, eTarget, eDraggable, event, ui, aSrc, aTarget, item, index) {
+            console.log("moved '" + item.name + "' from " + eSrc.data('model') + '[' + index + ']' + ' to ' + eTarget.data('model'));
+        };
     }]);
 
-    app.directive('draggable', function () {
+    app.directive('uiDraggable', function () {
         return {
             restrict:'A',
             link:function (scope, element, attrs) {
@@ -26,22 +53,38 @@
         };
     });
 
-    app.directive('droppable', function () {
+    app.directive('uiDroppable', function () {
+        var mv = function (eSrc, eTarget, eDraggable, event, ui, aSrc, aTarget, index, fnDropForbidden, fnOnDrop, fnOnDropRejected) {
+            var item = aSrc[index];
+            var error = !fnDropForbidden ? false : fnDropForbidden(eSrc, eTarget, eDraggable, event, ui, aSrc, aTarget, item, index);
+            if (!error) {
+                aTarget.push(item);
+                aSrc.splice(index, 1);
+                if (fnOnDrop) {
+                    fnOnDrop(eSrc, eTarget, eDraggable, event, ui, aSrc, aTarget, item, index);
+                }
+            } else {
+                if (fnOnDropRejected) {
+                    fnOnDropRejected(eSrc, eTarget, eDraggable, event, ui, aSrc, aTarget, item, index, error);
+                }
+            }
+        };
         return {
             restrict:'A',
-            link:function (scope, element, attrs) {
-                element.droppable({
+            link:function (scope, eTarget, attrs) {
+                eTarget.droppable({
                     drop:function (event, ui) {
                         scope.$apply(function () {
-                            var dragIndex = angular.element(ui.draggable).data('index'),
-                                reject = angular.element(ui.draggable).data('reject');
-
-                            if (element.hasClass('list2') && angular.element(ui.draggable).parent().hasClass('list1') && reject !== true) {
-                                scope.list2.push(scope.list1[dragIndex]);
-                                scope.list1.splice(dragIndex, 1);
-                            } else if (element.hasClass('list1') && angular.element(ui.draggable).parent().hasClass('list2') && reject !== true) {
-                                scope.list1.push(scope.list2[dragIndex]);
-                                scope.list2.splice(dragIndex, 1);
+                            var eDraggable = angular.element(ui.draggable);
+                            var eSrc = eDraggable.parent();
+                            var aTarget = scope.$eval(eTarget.data('model'));
+                            var aSrc = scope.$eval(eSrc.data('model'));
+                            var index = eDraggable.data('index');
+                            var fnOnDropComplete = scope.$eval(attrs.uiOnDropComplete);
+                            var fnOnDropRejected = scope.$eval(attrs.uiOnDropRejected);
+                            var fnDropForbidden = scope.$eval(attrs.uiDropForbidden);
+                            if (aSrc !== aTarget) {
+                                mv(eSrc, eTarget, eDraggable, event, ui, aSrc, aTarget, index, fnDropForbidden, fnOnDropComplete, fnOnDropRejected);
                             }
                         });
                     }
--- app/jsPaginatedList/controllers.js
+++ app/jsPaginatedList/controllers.js
@@ -1,32 +1,48 @@
-angular.module('paginatedListApp', [])
-.controller('paginatedListController', ['$scope', function($scope) {
-    $scope.currentPage = 0;
-    $scope.pageSize = 10;
-    $scope.data = [];
-    $scope.numberOfPages=function(){
-        return Math.ceil($scope.data.length/$scope.pageSize); 
-    }
-    for (var i=0; i<45; i++) {
-        $scope.data.push({name:'Item '+i, selected:false});
-    }
-    $scope.deleteSelected = function() {
-        $scope.data = _.reject($scope.data, function(item) {return item.selected;}); 
-    };
-    $scope.actionPossible = function() {
-	return _.some($scope.data, function(item) {return item.selected});
-    };
-    $scope.nextPage = function() {
-	$scope.data = _.map($scope.data, function(item) {return {name: item.name, selected: false};});
-        $scope.currentPage=$scope.currentPage+1;
-    };
-    $scope.previousPage = function() {
-	$scope.data = _.map($scope.data, function(item) {return {name: item.name, selected: false};});
-        $scope.currentPage=$scope.currentPage-1;
-    };
-}])
-.filter('startFrom', function() {
-    return function(input, start) {
-        start = +start; //parse to int
-        return input.slice(start);
-    }
-});
+(function () {
+
+    var app = angular.module('paginatedListApp', []);
+
+    app.controller('paginatedListController', ['$scope', function ($scope) {
+        $scope.currentPage = 0;
+        $scope.pageSize = 10;
+        $scope.data = [];
+        $scope.numberOfPages = function () {
+            return Math.ceil($scope.data.length / $scope.pageSize);
+        }
+        for (var i = 0; i < 45; i++) {
+            $scope.data.push({name:'Item ' + i, selected:false});
+        }
+        $scope.deleteSelected = function () {
+            var lengthBefore = $scope.data.length;
+            $scope.data = _.reject($scope.data, function (item) {
+                return item.selected;
+            });
+            humanMsg.displayMsg('<span>Deleted <strong>'+( lengthBefore-$scope.data.length)+'</strong> items</span>');
+        };
+        $scope.actionPossible = function () {
+            return _.some($scope.data, function (item) {
+                return item.selected
+            });
+        };
+        $scope.nextPage = function () {
+            $scope.data = _.map($scope.data, function (item) {
+                return {name:item.name, selected:false};
+            });
+            $scope.currentPage = $scope.currentPage + 1;
+        };
+        $scope.previousPage = function () {
+            $scope.data = _.map($scope.data, function (item) {
+                return {name:item.name, selected:false};
+            });
+            $scope.currentPage = $scope.currentPage - 1;
+        };
+    }]);
+
+    app.filter('startFrom', function () {
+        return function (input, start) {
+            start = +start; //parse to int
+            return input.slice(start);
+        }
+    });
+
+})();
